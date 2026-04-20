d9a63d5fd8fbceeab60abecd02e56b3b0dd818a4
Arjun <20230140120@students.ac.id>
2026-04-20 14:24:06 +0700

    menambahkan service User untuk handle logika CRUD

---
+++ src/main/java/com/PraktikumDB/deploy/service/UserService.java
@@ -0,0 +1,45 @@
+package com.PraktikumDB.deploy.service;
+
+import com.PraktikumDB.deploy.model.User;
+import com.PraktikumDB.deploy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request) {
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(String id, User request) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setName(request.getName());
+            user.setNim(request.getNim());
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
