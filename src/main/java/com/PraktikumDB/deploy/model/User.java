1ad42f642cfa987c0d5b0c62b4a84895011d6243
Arjun <20230140120@students.ac.id>
2026-04-20 14:14:54 +0700

    menambahkan entitas User dengan mapping tabel users

---
+++ src/main/java/com/PraktikumDB/deploy/model/User.java
@@ -0,0 +1,25 @@
+package com.PraktikumDB.deploy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table (name = "20230140120_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String nim;
+}
